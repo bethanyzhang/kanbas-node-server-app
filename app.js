9904347e06a6b644695671ac9cdf0636c7fa8e3c
Dan Zhang <zhang.dan4@northeastern.edu>
2023-12-05 13:21:07 -0800

    a6

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-
+// const express = require("express");
+import session from "express-session"
 
 import express from "express"
+import EnrollmentRoutes from "./enrollments/routes.js"
+import HelloRoutes from "./hello.js"
 import Lab5 from "./lab5.js"
-import cors from "cors"
 import CourseRoutes from "./courses/routes.js"
 import ModuleRoutes from "./modules/routes.js"
-import "dotenv/config"
+import UserRoutes from "./users/routes.js"
+import mongoose from "mongoose"
+import LikesRoutes from "./likes/routes.js"
+import FollowsRoutes from "./follows/routes.js"
+import SectionRoutes from "./sections/routes.js"
+import cors from "cors"
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas")
+
 
 const app = express()
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+)
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+}
+app.use(session(sessionOptions))
+
 app.use(express.json())
-app.use(cors())
 
+FollowsRoutes(app)
+LikesRoutes(app)
+UserRoutes(app)
 ModuleRoutes(app)
-
 CourseRoutes(app)
 Lab5(app)
-app.listen(process.env.PORT || 4000)
+HelloRoutes(app)
+SectionRoutes(app)
+EnrollmentRoutes(app)
+
+app.listen(4000)
